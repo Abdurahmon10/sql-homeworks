1f11ba3e7e43b5548919f1776edca9d5fb69a0d5
Abdurahmon <amuhammadjanov1010@gmail.com>
2025-03-26 13:12:01 +0500

    lesson-7 commit1

--- lesson-7/homework/tasks.sql
+++ lesson-7/homework/tasks.sql
@@ -54,6 +54,11 @@ INSERT INTO Customers VALUES
 (4, 'David'), 
 (5, 'Emma'), 
 (6, 'Frank');
+INSERT INTO Customers VALUES 
+(7, 'Grace'), 
+(8, 'Henry'), 
+(9, 'Isabella'), 
+(10, 'Jack');
 
 
 INSERT INTO Orders VALUES 
@@ -79,3 +84,103 @@ INSERT INTO Products VALUES
 (8, 'Headphones', 'Electronics'), 
 (9, 'Water Bottle', 'Accessories'), 
 (10, 'Backpack', 'Accessories');
+
+
+
+--task-1
+select * from customers as c left join
+orders as o on c.customerid=o.CustomerID;
+
+--task-2
+
+select * from customers as c left join
+	orders as o on c.customerid =o.CustomerID
+	where
+	o.orderid is null;
+
+--task-3
+select o.OrderID, p.productname from orders as o inner join orderdetails as od
+on o.OrderID=od.OrderID, products as p
+where p.ProductID=od.ProductID;
+
+
+
+--task-4
+select * from(	select c.CustomerName as cname ,count(o.orderid) coid from customers as c inner join
+	orders as o on c.customerid=o.CustomerID
+	group by c.CustomerName) as mytable
+	
+where coid>1;
+
+
+--task-5
+select * from(
+	select o.OrderID as oid, p.productname as pname, ROW_NUMBER() over(partition by o.orderid order by od.price) as price from orders as o inner join orderdetails as od
+	on o.OrderID=od.OrderID, products as p
+	where p.ProductID=od.ProductID) as mytable
+	where price=1;
+
+
+--task-6
+
+select * from(
+	select c.CustomerName, 
+    o.OrderID, 
+    o.OrderDate, 
+    p.ProductName, 
+    od.Quantity, 
+    od.Price,row_number() over(partition by c.customername order by o.orderdate desc) as date_rank from orders as o 
+	inner join orderdetails as od on o.OrderID=od.OrderID
+	inner join products as p on p.ProductID=od.ProductID
+	inner join customers as c on c.CustomerID=o.CustomerID
+	) as mytable
+	where date_rank=1;
+
+--task-7
+
+select cname,category from(
+select cname,string_agg(category,',') as category from(
+	select distinct p.category as category, 
+    --o.OrderID, 
+    --o.OrderDate, 
+    --p.ProductName, 
+	c.CustomerName as cname
+    --od.Quantity, 
+    --od.Price 
+	from orders as o 
+	inner join orderdetails as od on o.OrderID=od.OrderID
+	inner join products as p on p.ProductID=od.ProductID
+	inner join customers as c on c.CustomerID=o.CustomerID
+	) as mytable
+	group by 
+	cname) as mytable2
+	where category='electronics'
+	;
+
+
+--task-8
+select cname,category from(
+select cname,string_agg(category,',') as category from(
+	select distinct p.category as category, 
+    --o.OrderID, 
+    --o.OrderDate, 
+    --p.ProductName, 
+	c.CustomerName as cname
+    --od.Quantity, 
+    --od.Price 
+	from orders as o 
+	inner join orderdetails as od on o.OrderID=od.OrderID
+	inner join products as p on p.ProductID=od.ProductID
+	inner join customers as c on c.CustomerID=o.CustomerID
+	) as mytable
+	group by 
+	cname) as mytable2
+where category LIKE '%Stationery%';
+
+
+--task-9
+
+select c.customername, sum(od.price*od.quantity)  as total_spent from orders as o 
+	inner join orderdetails as od on o.OrderID=od.OrderID
+	inner join customers as c on c.CustomerID=o.CustomerID
+group by c.CustomerName;
